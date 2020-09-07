03e025bea2c4ed7ae57efab0d7d96e4c40580ecd
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-09-07 09:23:17 +0200

    trying again

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import Footer from "./Footer";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Projects from "./Projects";
 import About from "./About";
-import Cv from "./Cv";
+// import Cv from "./Cv";
 import Contact from "./Contact";
+import Cv from './Cv'
 
 
 const App = () => {
